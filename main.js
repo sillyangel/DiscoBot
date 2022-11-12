4618751c150e605e20e86cb38d898b2f1aea546d
sillyangel <sillyangel3@gmail.com>
2022-11-12 02:33:50 +0000

    see if work?

--- main.js
+++ main.js
@@ -46,3 +46,16 @@ client.on(Events.InteractionCreate, async interaction => {
 });
 
 client.login(token);
+
+// website
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
+});
