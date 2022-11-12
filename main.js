509c6c91f4358ef836f79c9f7f0dfbc2de764d71
sillyangel <sillyangel3@gmail.com>
2022-11-12 18:35:56 +0000

    last chance lol

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
-const { token } = require('./config.json');
+require('dotenv').config();
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -45,7 +45,7 @@ client.on(Events.InteractionCreate, async interaction => {
 	}
 });
 
-client.login(token);
+client.login(process.env.TOKEN);
 
 // website
 const express = require('express');
